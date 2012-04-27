acc4e15625617eba28b73e82fbe1d78f6904a29f
Christopher Buck <cmbuck@vt.edu>
2012-04-27 17:35:17 -0400

    Added scratch file to project

---
+++ src/scheduler/Scratch.java
@@ -0,0 +1,27 @@
+package scheduler;
+
+import java.util.List;
+import library.TimetableLibrary;
+import library.Section;
+
+public class Scratch
+{
+    static String timetableURL =
+        "https://banweb.banner.vt.edu/ssb/prod/HZSKVTSC.P_ProcRequest";
+
+    public static void main(String [] args)
+    {
+        TimetableLibrary tl = new TimetableLibrary(timetableURL);
+
+        List<Section> sections;
+
+        sections = tl.getSections("201201", "PHYS", "2305");
+
+        System.out.println(sections.size());
+        for (Section s : sections)
+        {
+            System.out.println(s.getCourse() + " : " + s.getInstructor() +
+                ", " + s.getDays() + ", " + s.getBegin() + " - " + s.getEnd());
+        }
+    }
+}
