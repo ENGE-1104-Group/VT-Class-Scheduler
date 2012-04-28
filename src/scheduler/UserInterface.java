cb00ffe3dcd71f4cbfdef149d16bc3a9a62aa52a
Christopher Buck <cmbuck@vt.edu>
2012-04-28 16:01:51 -0400

    Added some skeleton for UserInterface

--- src/scheduler/UserInterface.java
+++ src/scheduler/UserInterface.java
@@ -1,6 +1,39 @@
 package scheduler;
 
+/**
+ * // -------------------------------------------------------------------------
+/**
+ *  Contains all the code to handle displaying messages to the user and getting
+ *  the user's responses.
+ *
+ *  @author cmbuck
+ *  @version Apr 28, 2012
+ */
 public class UserInterface
 {
 
+    /**
+     * Displays the main menu of options to the user
+     */
+    public void displayMenu()
+    {
+
+    }
+
+    /**
+     * Gets the user input and returns it as a string
+     * @return the string of user input
+     */
+    public String getUserInput()
+    {
+
+    }
+
+    /**
+     * Handles the user input and calls the necessary methods
+     */
+    public void handleInput(String input)
+    {
+
+    }
 }
